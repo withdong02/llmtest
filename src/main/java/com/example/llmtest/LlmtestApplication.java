c229aa80b3677854bf4a2aaca6e4b5215ff0b74c
withdong02 <withdong02@gmail.com>
2025-03-26 12:12:24 +0800

    Initial commit

---
+++ src/main/java/com/example/llmtest/LlmtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.llmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/llmtest/LlmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
